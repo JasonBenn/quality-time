b733929bfb03532c87c75f890e423f4f9300ff39
Jason Benn <jasoncbenn@gmail.com>
2015-06-11 13:05:08 -0700

    Default Date is Time.now.

--- app/models/hangout.rb
+++ app/models/hangout.rb
@@ -1,3 +1,10 @@
 class Hangout < ActiveRecord::Base
   belongs_to :friend
+  validates :friend_id, presence: true
+
+  before_save :set_default_date_if_empty
+
+  def set_default_date_if_empty
+    date ||= Time.now
+  end
 end
